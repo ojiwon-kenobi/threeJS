3507fd84426bb830d9e0ee89caedb5554d722b08
Jiwon Lee <jiwon.lee@berkeley.edu>
2021-04-21 22:09:33 -0500

    run with python3 -m http.server

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const scene = new THREE.Scene() //where and what will be rendered
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+let scene, camera, renderer, torus;
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const torus = new THREE.Mesh( geometry, material );
-scene.add( torus );
-
-camera.position.z=50;
+function init(){
+    scene = new THREE.Scene() //where and what will be rendered
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    document.body.appendChild(renderer.domElement);
+    const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+    const texture = new THREE.TextureLoader().load('golden_marble.jpg');
+    const material = new THREE.MeshBasicMaterial({map:texture});
+    torus = new THREE.Mesh( geometry, material );
+    scene.add( torus );
+    
+    camera.position.z=50;
+}
 
 function animate(){
     requestAnimationFrame(animate);
@@ -20,4 +24,14 @@ function animate(){
     torus.rotation.y+=0.01;
     renderer.render(scene, camera);
 }
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth/ window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
 animate();
